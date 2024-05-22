578d50ee4902aeb11f5fe4e131d3d369b8c3ccfd
saiyanssj2 <40122911+saiyanssj2@users.noreply.github.com>
2024-05-22 14:39:21 +0700

    Update send_mail_jython2.7.1.py

--- send_mail_jython2.7.1.py
+++ send_mail_jython2.7.1.py
@@ -1,47 +1,32 @@
-from java.util import Properties
-from javax.mail import Session, Message
-from javax.mail.internet import InternetAddress, MimeMessage
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-# Cấu hình thông tin email
-mail_from = "mail@abc.com.vn"
-mail_to_list = ["minhhh@ssg.vn", "another_email@example.com"]
-mail_host = "10.X.Y.Z"
-mail_port = "25"
-use_authentication = False
-
-# Tạo đối tượng Properties để cấu hình
-properties = Properties()
-properties["mail.smtp.host"] = mail_host
-properties["mail.smtp.port"] = mail_port
-
-# Không sử dụng xác thực
-if not use_authentication:
-    properties["mail.smtp.auth"] = "false"
-
-# Tạo đối tượng Session
-session = Session.getDefaultInstance(properties)
-
-# Tạo đối tượng Message
-message = MimeMessage(session)
-message.setFrom(InternetAddress(mail_from))
-
-# Thêm nhiều người nhận
-for recipient_email in mail_to_list:
-    message.addRecipient(Message.RecipientType.TO, InternetAddress(recipient_email))
-
-message.setSubject("Test Email from Jython")
-
-# Nội dung HTML body
-html_content = """
-
+# Thông tin SMTP server
+smtp_server = 'mail.com'
+smtp_port = 25
+from_ = "mail@gmail.com"
+to_ = ['minhhh@gmail.com', 'abc@gmail.com']
+subject_ = 'Canh bao job treo'
+body_ = """
+Cac job dang bi treo, ngay du lieu, so luong hien tai:
+#PVCB.p_notify_job_lag
 """
 
-message.setContent(html_content, "text/html; charset=utf-8")
+# Tạo một thư mới
+msg = MIMEMultipart()
+msg['From'] = from_
+msg['To'] = ', '.join(to_)
+msg['Subject'] = subject_
 
-# Gửi email
-Transport = session.getTransport("smtp")
-Transport.connect(mail_host, mail_port)
-Transport.sendMessage(message, message.getAllRecipients())
-Transport.close()
+# Nội dung của thư
+msg.attach(MIMEText(body_, 'plain'))
 
-print("Email sent successfully.")
+# Kết nối đến SMTP server
+server = smtplib.SMTP(smtp_server, smtp_port)
+
+# Gửi thư đi
+server.sendmail(from_, to_, msg.as_string())
+
+# Đóng kết nối
+server.quit()
